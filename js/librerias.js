e390f5e05984bb10eb48a8118ef8e89e5f91d954
Max1miliano <maxipompas@hotmail.com>
2022-05-17 12:06:57 -0300

    agregando librerías

--- js/librerias.js
+++ js/librerias.js
@@ -5,9 +5,6 @@ let span = document.getElementById('userName');
 //operador ternario
 span.innerHTML = nombre.length > 1 ? nombre : "a Developer's Team";
 
-
-
-
 let inputUsuario = document.getElementById('userForm').length;
 let inputEmail = document.getElementById('emailForm').length;
 
@@ -26,21 +23,26 @@ function guardarDatos() {
     localStorage.setItem('nombreUser', valueUser);
     localStorage.setItem('emailUser', valueEmail);
 
-    // Operador AND 
-    valueUser != '' && alert('Inicio de sesión exitoso');
+    // Alert para logueo exitoso 
+    valueUser && valueEmail != '' && swal("Login", "Te logueaste correctamente", "success");
 
-    //Operador OR
-    alert(valueUser || 'Agrega tu email.');
+    //Alert para completar los 2 campos
+    valueEmail || swal({
+        title: "Datos Incorrectos",
+        text: "Tenes que completar todos los campos",
+        icon: "error",
+        button: "Cerrar",
+      });;
 
     let cerrarLogin = document.getElementById('loginContainer');
 
     //ACCESO CONDICIONAL A UN OBJETO
-    cerrarLogin.style.display = valueUser?.length > 1 ? 'none' :  'grid';
+    cerrarLogin.style.display = valueUser?.length > 1 ? valueEmail?.length > 1 ? 'none' : 'grid' :  'grid';
 
     // console.log(cerrarLogin?.style?.display);
 
     let habilitarScroll = document.getElementById('container');
-    habilitarScroll.style.overflow = valueUser?.length > 1 ? 'auto' :  'hidden';
+    habilitarScroll.style.overflow = valueUser?.length > 1 ? valueEmail?.length > 1 ? 'auto' : 'hidden' :  'hidden';
 }
 
 let agregarNombre = document.getElementById('nameContact');
@@ -61,5 +63,17 @@ function guardarComentarios() {
     let comentarios = document.getElementById('comentarios').value;
     localStorage.setItem('comentarios', comentarios);
 
-    alert('Gracias por contactarnos, te responderemos a la brevedad.');
+    // alert('Gracias por contactarnos, te responderemos a la brevedad.');
+    Toastify({
+        text: "Tu mensaje fue enviado correctamente",
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "left", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)",
+        },
+      }).showToast();
 }
